822e40815fa7267cd4c7434fb7d6be036c45cbca
Joy Brar <joybrar2001@gmail.com>
2024-03-28 01:49:46 +0530

    Added the features section

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import Highlights from "./components/Highlights"
 import Model from "./components/Model"
+import Features from "./components/Features"
 
 const App = () => {
   return (
     <main className="bg-black">
       <Navbar />
       <Hero />
-      <Highlights />     
+      <Highlights />
       <Model /> 
+      <Features />
     </main>
   )
 }
